27e625d1bcf5f56fed92a0a2f498ba3c1bbacc95
Urvish Prajapati <urvishprajapati412@gmail.com>
2025-12-18 23:02:47 -0500

    Refactor code

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <View className='flex flex-row justify-between items-center pt-10 bg-blue-500'>
       <Text className='h-auto pl-4 justify-center items-center'>Home</Text>
-      {/* <View className='flex-1 mt-5'> */}
+      
         <SearchBar />
-      {/* </View> */}
+      
       <Text className='h-auto pr-4 justify-center items-center'>Cart</Text>
 
     </View>
